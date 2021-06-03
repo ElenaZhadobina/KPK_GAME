9f2eae7d90416e7df1dc0f1553a5c72e39aca387
Elena <82103700+ElenaZhadobina@users.noreply.github.com>
2021-06-03 14:23:06 +0500

    Разделила на функции!

--- Sharik/TwoSharika_ZhElena.cpp
+++ Sharik/TwoSharika_ZhElena.cpp
@@ -1,7 +1,10 @@
-
 #include "TXLib.h"
 
 void MoveBall();
+
+void DrawBall (int x, int y, int r, COLORREF color, COLORREF fillColor);
+void ControlBall (int* x, int* y, int* vx, int* vy);
+void PhysicsBall (int* x, int* y, int* vx, int* vy, int r, int dt);
 
 int main()
     {
@@ -12,107 +15,87 @@ int main()
     return 0;
     }
 
+//----------------------------------------------------------------------
+
 void MoveBall()
     {
-    int x  = 50, y  = 50, x1 = 750, y1 = 550,
-        vx = 5,  vy = 3, vx1 = 5,  vy1 = 3,
-        r  = 20;
+    int  x1 = 50, y1 = 50,
+        vx1 = 5, vy1 = 3,
+         r1 = 20;
+
+    int  x2 = 750, y2 = 550,
+        vx2 = 5,  vy2 = 3,
+         r2 = 20;
 
     int dt = 1;
 
-    txSetColor     (RGB (x,   y,   100), 2);
-    txSetFillColor (RGB (x/2, y/2, 100));
-
-    while (! txGetAsyncKeyState (VK_ESCAPE))
+    while (!txGetAsyncKeyState (VK_ESCAPE))
         {
         //txSetFillColor (TX_BLACK);
         //txClear();
-        txCircle (x,  y,  r);
-        txCircle (x1, y1, r);
 
-        x  += vx  * dt;
-        y  += vy  * dt;
-        x1 += vx1 * dt;
-        y1 += vy1 * dt;
+        DrawBall (x1, y1, r1, RGB (x1, y1, 100), RGB (x1/2, y1/2, 100));
+        DrawBall (x2, y2, r2, RGB (x2, y2, 100), RGB (x2/2, y2/2, 100));
 
-        if (x > 800-r)
-            {
-            vx = -vx;
-            x = 800-r;
+        PhysicsBall (&x1, &y1, &vx1, &vy1, r1, dt);
+        PhysicsBall (&x2, &y2, &vx2, &vy2, r2, dt);
 
-            txSetColor     (RGB (x,   y,   50), 2);
-            txSetFillColor (RGB (x/2, y/2, 50));
-            }
+        ControlBall (&x1, &y1, &vx1, &vy1);
 
-        if (y > 600-r)
-            {
-            vy = -vy;
-            y = 600-r;
-
-            txSetColor     (RGB (x,   y,   100), 2);
-            txSetFillColor (RGB (x/2, y/2, 100));
-            }
-
-        if (x < 0 + r)
-            {
-            vx = -vx;
-            x = 0 + r;
-
-            txSetColor     (RGB (x,   y,   150), 2);
-            txSetFillColor (RGB (x/2, y/2, 150));
-            }
-
-        if (y < 0 + r)
-            {
-            vy = -vy;
-            y = 0 + r;
-
-            txSetColor     (RGB (x,   y,   200), 2);
-            txSetFillColor (RGB (x/2, y/2, 200));
-            }
-
-        if (txGetAsyncKeyState (VK_RIGHT)) x +=10;
-        if (txGetAsyncKeyState (VK_LEFT))  x -=10;
-        if (txGetAsyncKeyState (VK_UP))    y -=10;
-        if (txGetAsyncKeyState (VK_DOWN))  y +=10;
-
-        if (x1 > 800-r)
-            {
-            vx1 = -vx1;
-            x1 = 800-r;
-
-            txSetColor     (RGB (x1,   y1,   50), 2);
-            txSetFillColor (RGB (x1/2, y1/2, 50));
-            }
-
-        if (y1 > 600-r)
-            {
-            vy1 = -vy1;
-            y1 = 600-r;
-
-            txSetColor     (RGB (x1,   y1,   100), 2);
-            txSetFillColor (RGB (x1/2, y1/2, 100));
-            }
-
-        if (x1 < 0 + r)
-            {
-            vx1 = -vx1;
-            x1 = 0 + r;
-
-            txSetColor     (RGB (x1,   y1,   150), 2);
-            txSetFillColor (RGB (x1/2, y1/2, 150));
-            }
-
-        if (y1 < 0 + r)
-            {
-            vy1 = -vy1;
-            y1 = 0 + r;
-
-            txSetColor     (RGB (x1,   y1,   200), 2);
-            txSetFillColor (RGB (x1/2, y1/2, 200));
-            }
-
-        txSleep (5);
+        txSleep (10);
         }
     }
 
+//----------------------------------------------------------------------
+
+void DrawBall (int x, int y, int r, COLORREF color, COLORREF fillColor)
+    {
+    txSetColor (color, 2);
+    txSetFillColor (fillColor);
+
+    txCircle (x, y, r);
+    }
+
+//----------------------------------------------------------------------
+
+void ControlBall (int* x, int* y, int* vx, int* vy)
+    {
+    if (txGetAsyncKeyState (VK_RIGHT)) *x += 10;
+    if (txGetAsyncKeyState (VK_LEFT))  *x -= 10;
+    if (txGetAsyncKeyState (VK_UP))    *y -= 10;
+    if (txGetAsyncKeyState (VK_DOWN))  *y += 10;
+
+    if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+    }
+
+//----------------------------------------------------------------------
+
+void PhysicsBall (int* x, int* y, int* vx, int* vy, int r, int dt)
+    {
+    *x = *x + *vx * dt;
+    *y = *y + *vy * dt;
+
+    if (*x > 800 - r)
+        {
+        *vx = - *vx;
+        *x = 800 - r;
+        }
+
+    if (*y > 600 - r)
+        {
+        *vy = - *vy;
+        *y = 600 - r;
+        }
+
+    if (*x < 0 + r)
+        {
+        *vx = - *vx;
+        *x = 0 + r;
+        }
+
+    if (*y < 0 + r)
+        {
+        *vy = - *vy;
+        *y = 0 + r;
+        }
+    }
